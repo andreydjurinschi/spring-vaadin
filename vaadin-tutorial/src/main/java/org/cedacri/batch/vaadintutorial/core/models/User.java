f0d0c2837047e7337f4d0d9994039b0d0f480794
Djurinschi Andrei <andrei.djurinschi@iongroup.com>
2026-01-26 12:01:55 +0200

    proj-ini

---
+++ vaadin-tutorial/src/main/java/org/cedacri/batch/vaadintutorial/core/models/User.java
@@ -0,0 +1,79 @@
+package org.cedacri.batch.vaadintutorial.core.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// todo: additional entity logic and relationships
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+    @Column(nullable = false)
+    private String password;
+
+    public User(String fullName, String email, String login, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+    public User() {
+
+    }
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
